f5a9a31ea82db8e1475c49e69e3a21bab4cf318f
omikhail <ovchinnikov.m.v@internet.ru>
2024-10-29 12:34:00 +0300

    добавлен xui-install.sh

---
+++ xui-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Создаем папку в корневом каталоге
+echo "Создание папки /vpnserver-xui..."
+mkdir /vpnserver-xui
+
+# Переходим в созданную папку
+echo "Переход в папку /vpnserver-xui..."
+cd /vpnserver-xui
+
+# Клонируем репозиторий
+echo "Клонирование репозитория..."
+git clone https://github.com/Solovey-Tech/Private-Pattern.git .
+
+# Проверяем, успешно ли прошло клонирование
+if [ $? -eq 0 ]; then
+    echo "Репозиторий успешно склонирован."
+else
+    echo "Ошибка при клонировании репозитория."
+    exit 1
+fi
+
+# Запускаем vpn_installer.py
+echo "Запуск vpn_installer.py..."
+python3 vpn_installer/vpn_installer.py
+
+# Проверяем, успешно ли прошел запуск скрипта
+if [ $? -eq 0 ]; then
+    echo "vpn_installer.py успешно запущен."
+else
+    echo "Ошибка при запуске vpn_installer.py."
+    exit 1
+fi
